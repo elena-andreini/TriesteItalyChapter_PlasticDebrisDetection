d7321e2ff76c0f34b899f78bf1603d2d8be1b1c7
wilnesney <1075238+wilnesney@users.noreply.github.com>
2025-04-05 17:52:23 -0700

    Moved/edited a comment

--- task7-deployment/app.py
+++ task7-deployment/app.py
@@ -6,6 +6,8 @@ import streamlit as st
 st.title("Detecting Plastic Debris through Satellite Imagery in the Italian and Mediterranean Seas")
 
 def show_image_data(data):
+    # Note: Image reading adapted from 
+    # task4-baseline-modeling/UNet-patches-selection-experiment.ipynb
     bands_to_display = [4, 3, 2,]
     img = np.stack([data[band] for band in bands_to_display], axis=-1)
     img = (img - img.min()) / (img.max() - img.min())
@@ -17,7 +19,6 @@ def show_image_data(data):
 
 uploaded_file = st.file_uploader("Choose a satellite image file", type=["tif", "tiff"])
 if uploaded_file:
-    # Note: Image processing adapted from task4-baseline-modeling/UNet-patches-selection-experiment.ipynb
     with rasterio.open(uploaded_file) as file:
         data = file.read()
     
