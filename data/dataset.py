5b950311fb73cf51e7e0e7da888dca6ad59dfc66
Dhia Naouali <dhia12naouali@gmail.com>
2025-05-30 03:33:18 +0100

    added data utility functions and evaluation metrics, there is a bug in up until this commit

--- data/dataset.py
+++ data/dataset.py
@@ -5,33 +5,12 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 
-# MARIDA stats
-class_distr = np.array([0.00452, 0.00203, 0.00254, 0.00168, 0.00766, 0.15206, 0.20232,
- 0.35941, 0.00109, 0.20218, 0.03226, 0.00693, 0.01322, 0.01158, 0.00052])
-
-bands_mean = np.array([0.05197577, 0.04783991, 0.04056812, 0.03163572, 0.02972606, 0.03457443,
- 0.03875053, 0.03436435, 0.0392113,  0.02358126, 0.01588816]).astype(np.float32)
-
-bands_std = np.array([0.04725893, 0.04743808, 0.04699043, 0.04967381, 0.04946782, 0.06458357,
- 0.07594915, 0.07120246, 0.08251058, 0.05111466, 0.03524419]).astype(np.float32)
-
-MARIDA_LABELS = {
-    i: label for i, label in enumerate([
-        'Marine Debris', 'Dense Sargassum', 'Sparse Sargassum', 'Natural Organic Material',
-        'Ship', 'Clouds', 'Marine Water', 'Sediment-Laden Water', 'Foam', 'Turbid Water',
-        'Shallow Water', 'Waves', 'Cloud Shadows', 'Wakes', 'Mixed Water'
-    ], 1)
-}
-
-
-# download both datasets
-# create a unified splits txt files
-
 
 class MergedSegmentationDataset(Dataset):
     def __init__(
             self, 
             image_paths,
+            dataset_ids,
             band_means, 
             band_stds, 
             selected_bands=list(range(11)),
@@ -46,6 +25,7 @@ class MergedSegmentationDataset(Dataset):
         self._stds = band_stds[selected_bands]
         self._transform = transforms.ToTensor() if transform is None else transform
         self._standardization = standardization
+        self._dataset_ids = dataset_ids
         self._image_paths = image_paths
         self._impute = np.tile(self._means[:, np.newaxis, np.newaxis], (1, self._image_size, self._image_size))
         
@@ -58,22 +38,19 @@ class MergedSegmentationDataset(Dataset):
         invalid_mask = image == no_data
         invalid_mask |= np.isnan(image)
         invalid_mask |= image < -1.5
-        invalid_mask |= image > -1.5
+        invalid_mask |= image > 1.5
         return invalid_mask.astype(bool)
 
 
     def __getitem__(self, idx):
         image_path = self._image_paths[idx]
         mask_path = image_path.replace(".tif", "_cl.tif")
-        # return image_path, mask_path
         with rasterio.open(image_path) as src:
             image = src.read(list(self._bands + 1))
             invalid_mask = self.get_invalid_mask(image, src.nodata)
-
         with rasterio.open(mask_path) as src:
             mask = src.read().astype(int)
 
-        print(image.shape, mask.shape, invalid_mask.shape)
         mask[invalid_mask.any(axis=0, keepdims=True)] = 0 # as if unlabeled
         image[invalid_mask] = self._impute[invalid_mask]        
 
@@ -87,26 +64,6 @@ class MergedSegmentationDataset(Dataset):
 
         if self._standardization is not None:
             image = self._standardization(image)
-        
-        return image, mask
 
-
-    # utils ??
-
-    # create_LR_dataframe: split_path -> df: [image:mask]
-
-    # compute_fdi: tiff_path -> FDI : np.array
-
-    # cvt_to_fdi: images -> FDIs: np.array
-
-    # compute_ndwi: tiff_path -> ndwi: np.array
-
-    # plot_fdi: fdi,ndwi, img_path, mask_path ->
-
-    # cvt_rgb: 11bands_img -> rgb_img
-
-    # display: images, masks -> plt.plot 
-
-    # extract_date_tile: filename -> date, tile
-
-    # create_marida_df: data_path, mode -> pd.DataFrame({'image', 'mask', 'conf', 'date', 'tile'})
+        return image, mask, self._dataset_ids[idx]
+        return torch.from_numpy(image).float(), torch.from_numpy(mask).long(), self._dataset_ids[idx]
---
+++ data/utils.py
@@ -0,0 +1,98 @@
+import torch
+import random
+import torchvision.transforms.functional as vF
+
+
+class RandomRotationTransform:
+    def __init__(self, angles):
+        self.angles = angles
+
+    def __call__(self, x):
+        angle = random.choice(self.angles)
+        return vF.rotate(x, angle)
+
+
+def gen_weights(class_distribution, c = 1.02):
+    return 1/torch.log(c + class_distribution)
+
+
+def collate_fn(batch, device="cpu"):
+    images, masks, dataset_ids = zip(*batch)
+    images = torch.stack(images).to(device, non_blocking=True)
+    masks = torch.stack(masks).to(device, non_blocking=True)
+
+    dataset_ids = torch.tensor(dataset_ids, dtype=torch.long)
+
+    lr_masks = process_lwc(images, masks, dataset_ids, r1=4, r2=16, target_ratio=20, device=device)
+    marida_masks = process_marida(masks, dataset_ids, device=device)
+    masks = lr_masks + marida_masks
+    
+    return images, masks, dataset_ids
+
+
+
+def torch_dilate(mask, kernel_size, device='cpu'):
+    kernel = torch.ones(1, 1, kernel_size, kernel_size, device=device, dtype=torch.float32)
+    mask = mask.float().unsqueeze(1)
+    dilated = torch.nn.functional.conv2d(mask, kernel, padding=kernel_size // 2) > 0
+    return dilated.squeeze(1).bool()
+
+def process_lwc(images, masks, dataset_ids, r1=4, r2=16, target_ratio=20, threshold=None, device='cpu'):
+    bg_masks = torch.zeros_like(masks, dtype=torch.int64, device=device)
+    
+    valid_mask = (dataset_ids == 1)
+    if not valid_mask.any():
+        return bg_masks
+
+    # selecting lwc samples
+    selected_masks = masks[valid_mask]
+    bg_masks[valid_mask] = selected_masks * 2
+    
+    # annular ring
+    dilated_r1 = torch_dilate(selected_masks, 2 * r1 + 1, device=device)
+    dilated_r2 = torch_dilate(selected_masks, 2 * r2 + 1, device=device)
+    annular_masks = dilated_r2 & ~dilated_r1
+    
+    for idx in range(annular_masks.shape[0]):
+        valid_coords = torch.where(annular_masks[idx])
+        num_debris = torch.sum(selected_masks[idx] > 0).item()
+        num_background = min(len(valid_coords[0]), int(num_debris * target_ratio))
+        
+        if len(valid_coords[0]) > 0:
+            sample_indices = torch.randperm(len(valid_coords[0]), device=device)[:num_background]
+            bg_masks[valid_mask.nonzero(as_tuple=True)[0][idx], 
+                     valid_coords[0][sample_indices], 
+                     valid_coords[1][sample_indices]] = 1
+    
+    bg_masks[valid_mask] = bg_masks[valid_mask] - 1
+    return bg_masks
+
+
+
+def process_marida(masks, dataset_ids, device='cpu'):
+    marida_masks = torch.zeros_like(masks, dtype=torch.int64, device=device)
+    
+    marida_mask = (dataset_ids == 0)
+    if not marida_mask.any():
+        return marida_masks
+    
+    selected_masks = masks[marida_mask]
+    
+    # Set classes [1, 2, 3, 4, 9] to 2
+    debris_classes = torch.tensor([1, 2, 3, 4, 9], device=device)
+    is_debris = torch.isin(selected_masks, debris_classes)
+    marida_masks[marida_mask] = torch.where(
+        is_debris,
+        torch.tensor(2, dtype=torch.int64, device=device),
+        selected_masks
+    )
+
+    
+    marida_masks[marida_mask] = torch.where(
+        (marida_masks[marida_mask] != 0) & (marida_masks[marida_mask] != 2),
+        torch.tensor(1, dtype=torch.int64, device=device),
+        marida_masks[marida_mask]
+    )
+
+    marida_masks[marida_mask] = marida_masks[marida_mask] - 1
+    return marida_masks
